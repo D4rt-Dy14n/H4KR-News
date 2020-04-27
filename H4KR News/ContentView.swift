60eae2ad664a4a5b23d44796aec1410c8627efc1
D4rt-Dy14n <yuri.fedorov@inbox.ru>
2020-04-27 06:40:20 +0300

    Initial Commit

---
+++ H4KR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4KR News
+//
+//  Created by Юрий Федоров on 27.04.2020.
+//  Copyright © 2020 Юрий Федоров. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
